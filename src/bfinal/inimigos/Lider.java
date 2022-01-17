db5e46ae3b86355006a63aa227077f5f268b2cb7
gustavodesalles <gustavo_salles@gmail.com>
2022-01-16 23:18:33 -0300

    Finish battle system

--- src/bfinal/inimigos/Lider.java
+++ src/bfinal/inimigos/Lider.java
@@ -20,11 +20,19 @@ public class Lider extends Inimigo {
             System.out.println("O inimigo errou o ataque! Você não sofreu dano.");
         } else if (roll == 20) {
             int dano = this.getPontosAtk() + this.getArma().getPontosAtaque() + roll;
-            personagem.setPontosSaude(personagem.getPontosSaude() - dano);
+            if (dano > personagem.getPontosSaude()) {
+                personagem.setPontosSaude(0);
+            } else {
+                personagem.setPontosSaude(personagem.getPontosSaude() - dano);
+            }
             System.out.println("O inimigo acertou um ataque crítico! Você sofreu "+ dano +" de dano e agora possui "+ personagem.getPontosSaude() +" pontos de vida.");
         } else {
             int dano = this.getPontosAtk() + this.getArma().getPontosAtaque() + roll - personagem.getPontosDef();
-            personagem.setPontosSaude(personagem.getPontosSaude() - dano);
+            if (dano > personagem.getPontosSaude()) {
+                personagem.setPontosSaude(0);
+            } else {
+                personagem.setPontosSaude(personagem.getPontosSaude() - dano);
+            }
             System.out.println("O inimigo atacou! Você sofreu "+ dano +" de dano e agora possui "+ personagem.getPontosSaude() +" pontos de vida.");
         }
     }
